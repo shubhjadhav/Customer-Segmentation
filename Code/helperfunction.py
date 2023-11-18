cf1be2df491cd9e9084ea76e9ec6b2233daa06ec
JahnaviRamagiri <61361874+JahnaviRamagiri@users.noreply.github.com>
2023-11-18 00:22:07 -0500

    Added Code Sections

--- Code/helperfunction.py
+++ Code/helperfunction.py
@@ -1,9 +1,16 @@
 import pandas as pd
+import os
 
 data_path = "../Data/"
 
-
+def get_file_path(file_name):
+    dir_path = os.getcwd()
+    print("Current work Directory", dir_path)
+    file_path = dir_path + os.sep + file_name
+    print("File Path is ", file_path)
+    return file_path
 def create_df(file_name):
+    file_path = get_file_path(file_name)
     invoice_df = pd.read_csv(data_path + file_name + '.csv', encoding='unicode_escape')
     invoice_df['InvoiceDate'] = pd.to_datetime(invoice_df['InvoiceDate'])
     return invoice_df
