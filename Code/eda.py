c129fcd4667064ff72ee8697900f3b5afb83c9ab
Shubham Jadhav <shubhamjadhav1017@gmail.com>
2023-11-17 22:59:15 -0500

    Deleting EDA.py

--- Code/eda.py
+++
@@ -1,7 +0,0 @@
-import helperfunction as hf
-from tabulate import tabulate
-
-invoice_df = hf.create_df('invoice_data')
-
-print(f"\nTotal observations (records): {invoice_df.shape[0]}\n")
-print(tabulate(invoice_df.head(), headers='keys', tablefmt='psql'))
