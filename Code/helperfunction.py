3f275b0f7649840008733fc98779418ec958eec2
JahnaviRamagiri <61361874+JahnaviRamagiri@users.noreply.github.com>
2023-11-21 21:52:17 -0500

    Added Code for Data Cleaning

--- Code/helperfunction.py
+++ Code/helperfunction.py
@@ -14,3 +14,7 @@ def create_df(file_name):
     invoice_df = pd.read_csv(data_path + file_name + '.csv', encoding='unicode_escape')
     invoice_df['InvoiceDate'] = pd.to_datetime(invoice_df['InvoiceDate'])
     return invoice_df
+
+
+def print_observation(text):
+    print("OBSERVATION: ", text)
